13a7e7666e17965abd0372ccf603dd314e69f576
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-04-10 18:58:09 -0300

    Add support for Route and LOUFER_ROUTES in LoaferManager

--- loafer/manager.py
+++ loafer/manager.py
@@ -8,6 +8,7 @@ import os
 import signal
 
 from .conf import settings
+from .route import Route
 from .consumer import AsyncSQSConsumer
 
 logger = logging.getLogger(__name__)
@@ -25,11 +26,29 @@ class LoaferManager(object):
         self._executor = ThreadPoolExecutor(settings.MAX_THREAD_POOL)
         self._loop.set_default_executor(self._executor)
 
-    def start(self):
+    def get_routes(self, routes_values=None):
+        routes = []
+        if routes_values is None:
+            if not settings.LOAFER_ROUTES:
+                self.stop()
+                raise ValueError('Missing LOUFER_ROUTES configuration')
+        else:
+            for queue, handler in routes_values:
+                routes.append(Route(queue, handler))
+
+        # direct parameters takes precedence over configuration
+        if not routes:
+            for queue, handler in settings.LOAFER_ROUTES:
+                routes.append(Route(queue, handler))
+
+        return routes
+
+    def start(self, routes_values=None):
         logger.info('Starting Loafer (pid={}) ...'.format(os.getpid()))
 
+        routes = self.get_routes(routes_values)
         self._consumer = AsyncSQSConsumer()
-        self._future = asyncio.gather(self._consumer.consume(self._loop))
+        self._future = asyncio.gather(self._consumer.consume(routes))
 
         try:
             self._loop.run_forever()
