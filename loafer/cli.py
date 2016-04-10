b2af113b9923cf43645fae627f1a109141b3a693
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-04-10 20:22:55 -0300

    Add cli command to publish messages to sns and sqs

--- loafer/cli.py
+++ loafer/cli.py
@@ -7,6 +7,7 @@ import click
 
 from .conf import settings
 from .manager import LoaferManager
+from .publisher import Publisher
 from .utils import echo
 
 
@@ -32,3 +33,26 @@ def main():
 def cli(context):
     if context.invoked_subcommand is None:
         main()
+
+
+@cli.command()
+@click.option('--queue', default=None, help='SQS queue name ou url')
+@click.option('--topic', default=None, help='SNS topic name ou arn')
+@click.option('--msg', help='Message to publish (assumes json format)')
+def publish(queue, topic, msg):
+    # TODO: check the "click" way to validate parameters
+    if not (queue or topic):
+        raise click.UsageError('--queue or --topic parameter are missing')
+    if queue and topic:
+        raise click.UsageError('Use --queue OR --topic, not both')
+
+    publisher = Publisher()
+    if queue:
+        service = 'sqs'
+        destination = queue
+    if topic:
+        service = 'sns'
+        destination = topic
+
+    response = publisher.publish(service, destination, msg)
+    echo('Response: {}'.format(response))
