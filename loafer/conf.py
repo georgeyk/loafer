07e3cc3c4d974de375fbf73f82faa9a99e32fb86
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-04-13 12:08:44 -0300

    Enhance configuration organization, change consumer configuration to accept custom options

--- loafer/conf.py
+++ loafer/conf.py
@@ -5,20 +5,33 @@ from prettyconf import config
 
 
 class Settings(object):
+    # Logging
     LOAFER_LOGLEVEL = config('LOAFER_LOGLEVEL', default='WARNING')
     LOAFER_LOG_FORMAT = config('LOAFER_LOG_FORMAT',
                                default='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+    # Max concurrent jobs (asyncio)
     LOAFER_MAX_JOBS = config('LOAFER_MAX_JOBS', default=10)
+
+    # Default value are determined from the number of machine cores
     LOAFER_MAX_THREAD_POOL = config('LOAFER_MAX_THREAD_POOL', default=None)
-    # SQS long-polling
-    LOAFER_SQS_WAIT_TIME_SECONDS = config('LOAFER_SQS_WAIT_TIME_SECONDS', default=5)
-    # 10 is the maximum value
-    LOAFER_SQS_MAX_MESSAGES = config('LOAFER_SQS_MAX_MESSAGES', default=10)
 
-    LOAFER_ROUTES = [('test-images', 'loafer.jobs.async_example_job')]
+    # Consumer
 
+    # Currently, only AWS is supported, references:
+    # http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-long-polling.html
+    # http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html
+    # By default, SQS does not set long-polling (WaitTimeSeconds) and the MaxNumberOfMessages is 1
+    # TODO: tweak default values for acceptable performance
     LOAFER_DEFAULT_CONSUMER_CLASS = 'loafer.aws.consumer.Consumer'
+    LOAFER_DEFAULT_CONSUMER_OPTIONS = {'WaitTimeSeconds': 5,  # from 1-20
+                                       'MaxNumberOfMessages': 5}  # from 1-10
+
+    # Translator
+    LOAFER_DEFAULT_MESSAGE_TRANSLATOR_CLASS = 'loafer.message_translator.StringMessageTranslator'
+
+    # Routes
+    LOAFER_ROUTES = [('test-images', 'loafer.jobs.async_example_job')]
 
     def __init__(self, **defaults):
         if defaults:
