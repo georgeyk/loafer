1f035ecf220136e282897b40c38c0aec56a59d77
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-04-15 17:00:29 -0300

    Add examples jobs: reject_message_job and ignore_message_job

--- loafer/example/jobs.py
+++ loafer/example/jobs.py
@@ -3,7 +3,10 @@
 
 import logging
 
+from ..exceptions import RejectMessage, IgnoreMessage
+
 logger = logging.getLogger(__name__)
+
 
 # Job examples
 # Jobs are the units where the messages are sent as parameters
@@ -13,9 +16,21 @@ logger = logging.getLogger(__name__)
 def example_job(*args, **kwargs):
     message = 'Called example_job with args={} kwargs={}'.format(args, kwargs)
     logger.warning(message)
+    msg2 = 'args type is {} kwargs type is'.format(type(args), type(kwargs))
+    logger.warning(msg2)
 
 
 # The coroutine will be scheduled in the event loop
 async def async_example_job(*args, **kwargs):
     message = 'Called async_example_job with args={} kwargs={}'.format(args, kwargs)
     logger.warning(message)
+    msg2 = 'args type is {} kwargs type is'.format(type(args), type(kwargs))
+    logger.warning(msg2)
+
+
+async def reject_message_job(*args, **kwargs):
+    raise RejectMessage()
+
+
+async def ignore_message_job(*args, **kwargs):
+    raise IgnoreMessage()
--- tests/example/test_jobs.py
+++ tests/example/test_jobs.py
@@ -5,7 +5,9 @@ from unittest import mock
 
 import pytest
 
-from loafer.example.jobs import example_job, async_example_job
+from loafer.exceptions import RejectMessage, IgnoreMessage
+from loafer.example.jobs import (example_job, async_example_job,
+                                 reject_message_job, ignore_message_job)
 
 
 def test_example_job():
@@ -19,3 +21,15 @@ async def test_async_example_job():
     with mock.patch('loafer.example.jobs.logger') as mock_logger:
         await async_example_job()
         assert mock_logger.warning.called
+
+
+@pytest.mark.asyncio
+async def test_reject_message_job():
+    with pytest.raises(RejectMessage):
+        await reject_message_job()
+
+
+@pytest.mark.asyncio
+async def test_ignore_message_job():
+    with pytest.raises(IgnoreMessage):
+        await ignore_message_job()
