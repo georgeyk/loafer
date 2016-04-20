039082e318a3a324b0d01d1121914ac1a7a54483
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-04-19 22:34:57 -0300

    Refactor aws.message_translator tests

--- tests/aws/test_message_translator.py
+++ tests/aws/test_message_translator.py
@@ -9,18 +9,18 @@ from loafer.aws.message_translator import SQSMessageTranslator
 
 
 @pytest.fixture
-def translator():
+def sqs_translator():
     return SQSMessageTranslator()
 
 
-def test_translate(translator):
+def test_translate_sqs(sqs_translator):
     original = {'Body': json.dumps('some-content')}
-    content = translator.translate(original)
+    content = sqs_translator.translate(original)
     assert 'content' in content
     assert content['content'] == 'some-content'
 
     original = {'Body': json.dumps({'key': 'value'})}
-    content = translator.translate(original)
+    content = sqs_translator.translate(original)
     assert content['content'] == {'key': 'value'}
 
 
@@ -30,12 +30,12 @@ def parametrize_invalid_messages(request):
     return request.param
 
 
-def test_translate_handles_invalid_format(translator, parametrize_invalid_messages):
-    content = translator.translate(parametrize_invalid_messages)
+def test_translate_sqs_handles_invalid_format(sqs_translator, parametrize_invalid_messages):
+    content = sqs_translator.translate(parametrize_invalid_messages)
     assert content['content'] is None
 
 
-def test_translate_handles_json_error(translator):
+def test_translate_sqs_handles_json_error(sqs_translator):
     original = {'Body': 'invalid: json'}
-    content = translator.translate(original)
+    content = sqs_translator.translate(original)
     assert content['content'] is None
