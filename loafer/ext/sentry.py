5a9807e749ca20b4329c0eca2960136d1f976097
George Y. Kussumoto <georgeyk.dev@gmail.com>
2016-12-06 20:50:33 -0200

    Update sentry_handler

--- loafer/ext/sentry.py
+++ loafer/ext/sentry.py
@@ -1,19 +1,14 @@
-import asyncio
-
+# TODO: it should be async
 # https://github.com/getsentry/raven-aiohttp
 
 
-async def sentry_handler(client, loop=None):
-    loop = loop or asyncio.get_event_loop()
+def sentry_handler(client):
 
-    def send_to_sentry(job, *exc_info):
+    def send_to_sentry(exc_type, exc, message):
         client.captureException(
-            exc_info=exc_info,
-            extra={
-                'job_id': job.id,
-                'func': job.func_name,
-                'args': job.args,
-                'kwargs': job.kwargs,
-                'description': job.description,
-            }
+            extra={'message': message},
         )
+        # The message that originated the error will be acknowledged
+        return True
+
+    return send_to_sentry
