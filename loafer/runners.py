5a537bb1674f96a218c891d55a6c20a47c6771c1
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-01-07 21:09:41 -0200

    LoaferRunner.loop should be public to be shared

--- loafer/runners.py
+++ loafer/runners.py
@@ -11,28 +11,28 @@ class LoaferRunner:
 
     def __init__(self, loop=None, max_workers=None, on_stop_callback=None):
         self._on_stop_callback = on_stop_callback
-        self._loop = loop or asyncio.get_event_loop()
+        self.loop = loop or asyncio.get_event_loop()
 
         # XXX: See https://github.com/python/asyncio/issues/258
         # The minimum value depends on the number of cores in the machine
         # See https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
         self._executor = ThreadPoolExecutor(max_workers)
-        self._loop.set_default_executor(self._executor)
+        self.loop.set_default_executor(self._executor)
 
     def start(self, future=None, run_forever=True):
         start = 'Starting Loafer - (pid={} / run_forever={}) ...'
         logger.info(start.format(os.getpid(), run_forever))
 
-        self._loop.add_signal_handler(signal.SIGINT, self.stop)
-        self._loop.add_signal_handler(signal.SIGTERM, self.stop)
+        self.loop.add_signal_handler(signal.SIGINT, self.stop)
+        self.loop.add_signal_handler(signal.SIGTERM, self.stop)
 
         try:
             if run_forever:
-                self._loop.run_forever()
+                self.loop.run_forever()
             else:
-                self._loop.run_until_complete(future)
+                self.loop.run_until_complete(future)
         finally:
-            self._loop.close()
+            self.loop.close()
 
     def stop(self, *args, **kwargs):
         logger.info('Stopping Loafer ...')
@@ -40,5 +40,5 @@ class LoaferRunner:
             self._on_stop_callback()
 
         self._executor.shutdown(wait=True)
-        if self._loop.is_running():
-            self._loop.stop()
+        if self.loop.is_running():
+            self.loop.stop()
--- tests/test_runners.py
+++ tests/test_runners.py
@@ -6,26 +6,26 @@ from loafer.runners import LoaferRunner
 def test_runner_start():
     runner = LoaferRunner(loop=mock.Mock())
     runner.start()
-    assert runner._loop.run_forever.called
-    assert runner._loop.close.called
+    assert runner.loop.run_forever.called
+    assert runner.loop.close.called
 
 
 def test_runner_start_run_until_complete():
     runner = LoaferRunner(loop=mock.Mock())
     runner.start(run_forever=False)
-    assert runner._loop.run_until_complete.called
-    assert runner._loop.close.called
+    assert runner.loop.run_until_complete.called
+    assert runner.loop.close.called
 
 
 def test_runner_stop():
     runner = LoaferRunner(loop=mock.Mock())
     runner.stop()
-    assert runner._loop.stop.called
+    assert runner.loop.stop.called
 
 
 def test_runner_stop_with_callback():
     callback = mock.Mock()
     runner = LoaferRunner(loop=mock.Mock(), on_stop_callback=callback)
     runner.stop()
-    assert runner._loop.stop.called
+    assert runner.loop.stop.called
     assert callback.called
