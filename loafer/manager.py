37b2a14e55f725945570715d163334e1fa9208ae
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-01-07 19:44:24 -0200

    Refactor LoaferManager to use LoaferRunner
    The code responsible to handle event loop was moved to LoaferRunner.
    We also remove the checks of routes and consumers construction and it
    will be rewritten in the future.
    LoaferManager will be responsible to handle LoaferDispatcher and start
    the execution via LoaferRunner.

--- loafer/manager.py
+++ loafer/manager.py
@@ -1,88 +1,45 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 import logging
-import os
-import signal
 
 from cached_property import cached_property
 
-from .conf import settings
 from .dispatcher import LoaferDispatcher
-from .exceptions import ConfigurationError
-from .routes import Route
+from .runners import LoaferRunner
 
 logger = logging.getLogger(__name__)
 
 
 class LoaferManager:
 
-    def __init__(self, configuration=None):
-        self._conf = configuration or settings
-
-        self._loop = asyncio.get_event_loop()
-        self._loop.add_signal_handler(signal.SIGINT, self.stop)
-        self._loop.add_signal_handler(signal.SIGTERM, self.stop)
-
-        # XXX: See https://github.com/python/asyncio/issues/258
-        # The minimum value depends on the number of cores in the machine
-        # See https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
-        self._executor = ThreadPoolExecutor(self._conf.LOAFER_MAX_THREAD_POOL)
-        self._loop.set_default_executor(self._executor)
-
-    @cached_property
-    def routes(self):
-        if not self._conf.LOAFER_ROUTES:
-            msg = 'Missing LOAFER_ROUTES configuration'
-            logger.critical(msg)
-            if self._loop.is_running():
-                self.stop()
-            raise ConfigurationError(msg)
-
-        routes = []
-        for data in self._conf.LOAFER_ROUTES:
-            message_translator = data.get('message_translator', None)
-            routes.append(Route(data['source'], data['handler'], data['name'],
-                                message_translator=message_translator))
-        return routes
-
-    @cached_property
-    def consumers(self):
-        return self._conf.LOAFER_CONSUMERS or []
+    def __init__(self, routes, consumers, runner=None):
+        self.runner = runner or LoaferRunner(on_stop_callback=self.on_loop__stop)
+        self.routes = routes
+        self.consumers = consumers
 
     @cached_property
     def dispatcher(self):
-        if self._conf.LOAFER_ROUTES:
-            return LoaferDispatcher(self.routes, self.consumers)
+        return LoaferDispatcher(self.routes, self.consumers)
 
-    def start(self):
-        start = 'Starting Loafer - (pid={}) ...'
-        logger.info(start.format(os.getpid()))
-
-        self._future = asyncio.gather(self.dispatcher.dispatch_consumers())
+    def run(self, forever=True):
+        self._future = asyncio.gather(self.dispatcher.dispatch_providers())
         self._future.add_done_callback(self.on_future__errors)
+        self.runner.start(self._future, run_forever=forever)
 
-        try:
-            self._loop.run_forever()
-        finally:
-            self._loop.close()
-
-    def stop(self, *args, **kwargs):
-        logger.info('Stopping Loafer ...')
-
-        logger.debug('Stopping consumers ...')
-        if self.dispatcher:
-            self.dispatcher.stop_consumers()
-
-            logger.debug('Cancel schedulled operations ...')
-            self._future.cancel()
-
-        logger.debug('Waiting to shutdown ...')
-        self._executor.shutdown(wait=True)
-        self._loop.stop()
+    #
+    # Callbacks
+    #
 
     def on_future__errors(self, future):
         exc = future.exception()
         # Unhandled errors crashes the event loop execution
         if isinstance(exc, BaseException):
             logger.critical('Fatal error caught: {!r}'.format(exc))
-            self.stop()
+            self.runner.stop()
+
+    def on_loop__stop(self, *args, **kwargs):
+        logger.debug('Stopping consumers ...')
+        self.dispatcher.stop_providers()
+
+        if hasattr(self, '_future'):
+            logger.debug('Cancel schedulled operations ...')
+            self._future.cancel()
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,84 +1,35 @@
 import asyncio
 from unittest import mock
 
-import pytest
-
-from loafer.conf import Settings
-from loafer.exceptions import ConfigurationError, ProviderError
 from loafer.dispatcher import LoaferDispatcher
+from loafer.exceptions import ProviderError
 from loafer.manager import LoaferManager
+from loafer.runners import LoaferRunner
 
 
-@pytest.mark.asyncio
-async def test_routes():
-    manager = LoaferManager()
-    assert manager.routes
-    assert len(manager.routes) == 1
-    # this value is set on envvar in pytest.ini
-    assert manager.routes[0].name == 'test'
-
-
-@pytest.mark.asyncio
-async def test_routes_raise_error_if_not_configured():
-    settings = Settings()
-    settings.LOAFER_ROUTES = None
-
-    with pytest.raises(ConfigurationError):
-        manager = LoaferManager(settings)
-        manager.routes
-
-    settings.LOAFER_ROUTES = []
-
-    with pytest.raises(ConfigurationError):
-        manager = LoaferManager(settings)
-
-        manager.routes
-
-
-@pytest.mark.asyncio
-async def test_routes_stop_manager_if_loop_is_running():
-    settings = Settings()
-    settings.LOAFER_ROUTES = None
-    manager = LoaferManager(settings)
-    manager.stop = mock.Mock()
-
-    with mock.patch.object(manager._loop, 'is_running', return_value=True):
-        with pytest.raises(ConfigurationError):
-            manager.routes
-
-        assert manager.stop.called
-        assert manager.stop.called_once_with()
-
-
-@pytest.mark.asyncio
-async def test_consumers():
-    manager = LoaferManager()
-    assert manager.consumers
-    assert len(manager.consumers) == 1
-
-
-@pytest.mark.asyncio
-async def test_consumers_returns_empty_if_not_configure():
-    settings = Settings()
-    settings.LOAFER_CONSUMERS = None
-    manager = LoaferManager(settings)
-    assert manager.consumers == []
-
-
-@pytest.mark.asyncio
-async def test_dispatcher():
-    manager = LoaferManager()
+def test_dispatcher():
+    runner = LoaferRunner(loop=mock.Mock())
+    manager = LoaferManager(routes=[], consumers=[], runner=runner)
     assert manager.dispatcher
     assert isinstance(manager.dispatcher, LoaferDispatcher)
 
 
-@pytest.mark.asyncio
-async def test_on_future_errors():
-    manager = LoaferManager()
-    manager.stop = mock.Mock()
+def test_on_future_errors():
+    manager = LoaferManager(routes=[], consumers=[])
+    manager.runner = mock.Mock()
     future = asyncio.Future()
     future.set_exception(ProviderError)
     manager.on_future__errors(future)
 
-    assert manager.stop.called
-    assert manager.stop.called_once_with()
+    assert manager.runner.stop.called
+    assert manager.runner.stop.called_once_with()
+
+
+def test_on_loop__stop():
+    manager = LoaferManager(routes=[], consumers=[])
+    manager.dispatcher = mock.Mock()
+    manager._future = mock.Mock()
+    manager.on_loop__stop()
+
+    assert manager.dispatcher.stop_providers.called
+    assert manager._future.cancel.called
