20106a004e6a9df8c0d8b0eab7481355f2bd1368
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-03-25 17:23:01 -0300

    SQSRoute/SNSQueueRoute: setup SQSProvider, set message_translator and name if not configured

--- loafer/ext/aws/routes.py
+++ loafer/ext/aws/routes.py
@@ -1,14 +1,29 @@
 from ...routes import Route
+from .providers import SQSProvider
 from .message_translators import SQSMessageTranslator, SNSMessageTranslator
 
 
 class SQSRoute(Route):
-    def __init__(self, *args, **kwargs):
-        kwargs['message_translator'] = SQSMessageTranslator()
+    def __init__(self, provider_queue, provider_options=None, *args, **kwargs):
+        provider_options = provider_options or {}
+        provider = SQSProvider(provider_queue, **provider_options)
+        kwargs['provider'] = provider
+        if not kwargs.get('message_translator', False):
+            kwargs['message_translator'] = SQSMessageTranslator()
+        if not kwargs.get('name', False):
+            kwargs['name'] = provider_queue
+
         super().__init__(*args, **kwargs)
 
 
 class SNSQueueRoute(Route):
-    def __init__(self, *args, **kwargs):
-        kwargs['message_translator'] = SNSMessageTranslator()
+    def __init__(self, provider_queue, provider_options=None, *args, **kwargs):
+        provider_options = provider_options or {}
+        provider = SQSProvider(provider_queue, **provider_options)
+        kwargs['provider'] = provider
+        if not kwargs.get('message_translator', False):
+            kwargs['message_translator'] = SNSMessageTranslator()
+        if not kwargs.get('name', False):
+            kwargs['name'] = provider_queue
+
         super().__init__(*args, **kwargs)
--- tests/ext/aws/test_routes.py
+++ tests/ext/aws/test_routes.py
@@ -1,12 +1,39 @@
-from loafer.ext.aws.routes import SQSRoute, SNSQueueRoute
+from unittest import mock
+
 from loafer.ext.aws.message_translators import SQSMessageTranslator, SNSMessageTranslator
+from loafer.ext.aws.providers import SQSProvider
+from loafer.ext.aws.routes import SQSRoute, SNSQueueRoute
 
 
 def test_sqs_route():
-    route = SQSRoute('what', 'ever')
+    route = SQSRoute('what', handler='ever')
     assert isinstance(route.message_translator, SQSMessageTranslator)
+    assert isinstance(route.provider, SQSProvider)
+    assert route.name == 'what'
+
+
+def test_sqs_route_keep_message_translator():
+    route = SQSRoute('what', handler='ever', message_translator=mock.Mock())
+    assert isinstance(route.message_translator, mock.Mock)
+
+
+def test_sqs_route_keep_name():
+    route = SQSRoute('what', handler='ever', name='foobar')
+    assert route.name == 'foobar'
 
 
 def test_sns_queue_route():
-    route = SNSQueueRoute('what', 'ever')
+    route = SNSQueueRoute('what', handler='ever')
     assert isinstance(route.message_translator, SNSMessageTranslator)
+    assert isinstance(route.provider, SQSProvider)
+    assert route.name == 'what'
+
+
+def test_sns_queue_route_keep_message_translator():
+    route = SNSQueueRoute('what', handler='ever', message_translator=mock.Mock())
+    assert isinstance(route.message_translator, mock.Mock)
+
+
+def test_sns_queue_route_keep_name():
+    route = SNSQueueRoute('what', handler='ever', name='foobar')
+    assert route.name == 'foobar'
