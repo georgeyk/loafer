79365d5d9d5ea8fcb2be42b946252963a55e0521
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-04-09 17:26:45 -0300

    Update package metadata plus minor enhancements

--- setup.py
+++ setup.py
@@ -43,14 +43,23 @@ with codecs.open(os.path.join(here, 'CHANGES.rst'), encoding='utf-8') as f:
 # Requirements
 
 # Unduplicated tests_requirements and requirements/test.txt
-tests_requirements = ['pytest', 'pytest-asyncio', 'pytest-cov', 'coveralls',
-                      'asynctest', 'tox']
+tests_requirements = [
+    'pytest',
+    'pytest-asyncio',
+    'pytest-cov',
+    'coveralls',
+    'asynctest',
+    'flake8',
+    'tox'
+]
 
 # aiohttp version are directly tied with aiobotocore, please check before update it.
-install_requirements = ['aiohttp>=2.0.4',
-                        'boto3>=1.3.0',
-                        'aiobotocore>=0.3.0',
-                        'cached-property>=1.3.0']
+install_requirements = [
+    'aiohttp>=2.0.4',
+    'boto3>=1.3.0',
+    'aiobotocore>=0.3.0',
+    'cached-property>=1.3.0',
+]
 
 
 # setup
@@ -75,9 +84,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Distributed Computing',
     ],
-    keywords='asynchronous asyncio message dispatcher tasks',
+    keywords='asynchronous asyncio message dispatcher tasks microservices',
     setup_requires=['pytest-runner'],
     install_requires=install_requirements,
     tests_require=tests_requirements,
