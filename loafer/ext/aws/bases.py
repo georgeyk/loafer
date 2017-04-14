65b1e6456cdd095584dc13c8deaf2fb95523e6a2
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-04-14 18:04:15 -0300

    Add BaseSNSClient

--- loafer/ext/aws/bases.py
+++ loafer/ext/aws/bases.py
@@ -26,3 +26,26 @@ class BaseSQSClient:
             self._cached_queue_urls[queue_name] = response['QueueUrl']
 
         return self._cached_queue_urls[queue_name]
+
+
+class BaseSNSClient:
+    def __init__(self, endpoint_url=None, use_ssl=True, loop=None):
+        self.endpoint_url = endpoint_url
+        self.use_ssl = use_ssl
+        self._loop = loop or asyncio.get_event_loop()
+        self._cached_topic_arns = {}
+
+    @cached_property
+    def client(self):
+        session = aiobotocore.get_session(loop=self._loop)
+        return session.create_client('sns', endpoint_url=self.endpoint_url, use_ssl=self.use_ssl)
+
+    async def get_topic_arn(self, topic_name):
+        if topic_name not in self._cached_topic_arns:
+            topics = await self.client.list_topics()
+            for topic_data in topics['Topics']:
+                arn = topic_data['TopicArn']
+                name = arn.split(':')[-1]
+                self._cached_topic_arns[name] = arn
+
+        return self._cached_topic_arns[topic_name]
--- tests/ext/aws/test_bases.py
+++ tests/ext/aws/test_bases.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import pytest
 
-from loafer.ext.aws.bases import BaseSQSClient
+from loafer.ext.aws.bases import BaseSQSClient, BaseSNSClient
 
 
 @pytest.fixture
@@ -31,3 +31,31 @@ async def test_cache_get_queue_url(mock_boto_session_sqs, boto_client_sqs, base_
         assert queue_url.startswith('https://')
         assert queue_url.endswith('queue-name')
         assert boto_client_sqs.get_queue_url.call_count == 1
+
+
+@pytest.fixture
+def base_sns_client():
+    return BaseSNSClient()
+
+
+@pytest.mark.asyncio
+async def test_get_topic_arn(mock_boto_session_sns, boto_client_sns, base_sns_client):
+    with mock_boto_session_sns as mock_sns:
+        arn = await base_sns_client.get_topic_arn('topic-name')
+        assert arn.startswith('arn:')
+        assert arn.endswith('topic-name')
+
+        assert mock_sns.called
+        assert mock_sns.called_once_with('sns')
+        assert boto_client_sns.list_topics.called
+        assert boto_client_sns.called_once_with()
+
+
+@pytest.mark.asyncio
+async def test_cache_get_topic_arn(mock_boto_session_sns, boto_client_sns, base_sns_client):
+    with mock_boto_session_sns:
+        await base_sns_client.get_topic_arn('topic-name')
+        arn = await base_sns_client.get_topic_arn('topic-name')
+        assert arn.startswith('arn:')
+        assert arn.endswith('topic-name')
+        assert boto_client_sns.list_topics.call_count == 1
