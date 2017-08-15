6e214e3101e490ab0303af58b6b39cf95698fc49
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-08-15 19:11:32 -0300

    Add tests using provider_options parameter for loafer.ext.aws.routes

--- tests/ext/aws/test_routes.py
+++ tests/ext/aws/test_routes.py
@@ -22,6 +22,12 @@ def test_sqs_route_keep_name(dummy_handler):
     assert route.name == 'foobar'
 
 
+def test_sqs_route_provider_options(dummy_handler):
+    route = SQSRoute('what', {'use_ssl': False}, handler=dummy_handler, name='foobar')
+    assert 'use_ssl' in route.provider._client_options
+    assert route.provider._client_options['use_ssl'] is False
+
+
 def test_sns_queue_route(dummy_handler):
     route = SNSQueueRoute('what', handler=dummy_handler)
     assert isinstance(route.message_translator, SNSMessageTranslator)
@@ -39,3 +45,9 @@ def test_sns_queue_route_keep_message_translator(dummy_handler):
 def test_sns_queue_route_keep_name(dummy_handler):
     route = SNSQueueRoute('what', handler=dummy_handler, name='foobar')
     assert route.name == 'foobar'
+
+
+def test_sns_queue_route_provider_options(dummy_handler):
+    route = SNSQueueRoute('what', provider_options={'region_name': 'sa-east-1'}, handler=dummy_handler, name='foobar')
+    assert 'region_name' in route.provider._client_options
+    assert route.provider._client_options['region_name'] == 'sa-east-1'
