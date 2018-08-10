29e1da7ecf5426a608843c8379950f2e47418b55
George Y. Kussumoto <georgeyk.dev@gmail.com>
2018-08-10 17:09:33 -0300

    Add python3.7 as supported version

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Distributed Computing',
     ],
     keywords='asynchronous asyncio message dispatcher tasks microservices',
