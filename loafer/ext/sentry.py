5415657ec9aaa516fb2aa81935e9279d933df73b
George Y. Kussumoto <georgeyk.dev@gmail.com>
2020-05-01 20:30:48 -0300

    Initial sentry wrapper support for new sdk version

--- loafer/ext/sentry.py
+++ loafer/ext/sentry.py
@@ -2,13 +2,12 @@
 # https://github.com/getsentry/raven-aiohttp
 
 
-def sentry_handler(client, delete_message=False):
+def sentry_handler(capture_exception, delete_message=False):
 
     def send_to_sentry(exc_info, message):
-        client.captureException(
-            exc_info,
-            extra={'message': message},
-        )
+        scope_kwargs = {"message": message}
+        capture_exception(exc_info, **scope_kwargs)
+
         return delete_message
 
     return send_to_sentry
