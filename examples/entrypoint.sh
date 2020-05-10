50614be01f361a221225384f129851f691fdd620
George Y. Kussumoto <georgeyk.dev@gmail.com>
2020-05-10 18:53:51 -0300

    Improve docker setup

---
+++ examples/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+aws sns create-topic --endpoint-url=${GOAWS_URL} --name=loafer__notification
+topic=$(aws sns list-topics --endpoint-url=${GOAWS_URL} | grep loafer | cut -d\" -f4)
+
+aws sqs create-queue --endpoint-url=${GOAWS_URL} --queue-name=echo__loafer__notification
+queue_url=$(aws sqs list-queues --endpoint-url=${GOAWS_URL} | grep loafer | cut -d\" -f2)
+
+aws sns subscribe --endpoint-url=${GOAWS_URL} --topic-arn ${topic} --protocol sqs --notification-endpoint ${queue_url}
+
+exec python -m examples.echo
