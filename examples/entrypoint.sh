2dc0113ceeee1652be20f4d1d351da903ea72899
George Y. Kussumoto <georgeyk.dev@gmail.com>
2020-05-16 16:58:37 -0300

    Examples: publish test message

--- examples/entrypoint.sh
+++ examples/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -eux
 
 aws sns create-topic --endpoint-url=${GOAWS_URL} --name=loafer__notification
 topic=$(aws sns list-topics --endpoint-url=${GOAWS_URL} | grep loafer | cut -d\" -f4)
@@ -9,4 +9,5 @@ queue_url=$(aws sqs list-queues --endpoint-url=${GOAWS_URL} | grep loafer | cut 
 
 aws sns subscribe --endpoint-url=${GOAWS_URL} --topic-arn ${topic} --protocol sqs --notification-endpoint ${queue_url}
 
+aws sns publish --endpoint-url=${GOAWS_URL} --topic-arn ${topic} --message file://examples/sample.json
 exec python -m examples.echo
