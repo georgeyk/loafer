64857feeee681ebb410d5c70a0d1024c6fe207c7
Christian Hartung <hartung@live.com>
2020-05-26 13:41:57 -0300

    Fix sentry_sdk integration

--- loafer/ext/sentry.py
+++ loafer/ext/sentry.py
@@ -1,12 +1,12 @@
 # TODO: it should be async
 
 
-def sentry_handler(capture_exception, delete_message=False):
+def sentry_handler(sdk_or_hub, delete_message=False):
 
     def send_to_sentry(exc_info, message):
-        scope_kwargs = {"message": message}
-        capture_exception(exc_info, **scope_kwargs)
-
+        with sdk_or_hub.push_scope() as scope:
+            scope.set_extra("message", message)
+            sdk_or_hub.capture_exception(exc_info)
         return delete_message
 
     return send_to_sentry
--- tests/ext/test_sentry.py
+++ tests/ext/test_sentry.py
@@ -3,29 +3,48 @@ from unittest import mock
 from loafer.ext.sentry import sentry_handler
 
 
+class MockScope:
+    def __init__(self):
+        self.set_extra = mock.Mock()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return None
+
+
 def test_sentry_handler():
-    capture_exception_mocked = mock.Mock()
-    handler = sentry_handler(capture_exception_mocked)
-    exc = ValueError('test')
+    mock_scope = MockScope()
+    sdk_mocked = mock.Mock()
+    sdk_mocked.push_scope.return_value = mock_scope
+
+    handler = sentry_handler(sdk_mocked)
+    exc = ValueError("test")
     exc_info = (type(exc), exc, None)
-    delete_message = handler(exc_info, 'test')
+    delete_message = handler(exc_info, "test")
 
     assert delete_message is False
-    assert capture_exception_mocked.called
-    capture_exception_mocked.assert_called_once_with(
-        exc_info, message='test',
+    assert sdk_mocked.push_scope.called
+    mock_scope.set_extra.assert_called_once_with(
+        "message", "test"
     )
+    sdk_mocked.capture_exception.assert_called_once_with(exc_info)
 
 
 def test_sentry_handler_delete_message():
-    capture_exception_mocked = mock.Mock()
-    handler = sentry_handler(capture_exception_mocked, delete_message=True)
-    exc = ValueError('test')
+    mock_scope = MockScope()
+    sdk_mocked = mock.Mock()
+    sdk_mocked.push_scope.return_value = mock_scope
+
+    handler = sentry_handler(sdk_mocked, delete_message=True)
+    exc = ValueError("test")
     exc_info = (type(exc), exc, None)
-    delete_message = handler(exc_info, 'test')
+    delete_message = handler(exc_info, "test")
 
     assert delete_message is True
-    assert capture_exception_mocked.called
-    capture_exception_mocked.assert_called_once_with(
-        exc_info, message='test',
+    assert sdk_mocked.push_scope.called
+    mock_scope.set_extra.assert_called_once_with(
+        "message", "test"
     )
+    sdk_mocked.capture_exception.assert_called_once_with(exc_info)
