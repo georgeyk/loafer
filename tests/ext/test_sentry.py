8208bd54f9e2cc4faa3d009651ef46a617f25315
Christian Hartung <christian.hartung@olist.com>
2020-07-16 16:57:00 -0300

    Improve tests

--- tests/ext/test_sentry.py
+++ tests/ext/test_sentry.py
@@ -3,19 +3,8 @@ from unittest import mock
 from loafer.ext.sentry import sentry_handler
 
 
-class MockScope:
-    def __init__(self):
-        self.set_extra = mock.Mock()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        return None
-
-
 def test_sentry_handler():
-    mock_scope = MockScope()
+    mock_scope = mock.MagicMock()
     sdk_mocked = mock.Mock()
     sdk_mocked.push_scope.return_value = mock_scope
 
@@ -26,14 +15,14 @@ def test_sentry_handler():
 
     assert delete_message is False
     assert sdk_mocked.push_scope.called
-    mock_scope.set_extra.assert_called_once_with(
+    mock_scope.__enter__.return_value.set_extra.assert_called_once_with(
         "message", "test"
     )
     sdk_mocked.capture_exception.assert_called_once_with(exc_info)
 
 
 def test_sentry_handler_delete_message():
-    mock_scope = MockScope()
+    mock_scope = mock.MagicMock()
     sdk_mocked = mock.Mock()
     sdk_mocked.push_scope.return_value = mock_scope
 
@@ -44,7 +33,7 @@ def test_sentry_handler_delete_message():
 
     assert delete_message is True
     assert sdk_mocked.push_scope.called
-    mock_scope.set_extra.assert_called_once_with(
+    mock_scope.__enter__.return_value.set_extra.assert_called_once_with(
         "message", "test"
     )
     sdk_mocked.capture_exception.assert_called_once_with(exc_info)
